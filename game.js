161c5cf1b27ab65716f6c771587d4b83032e8bb8
ErRenzo <renzo.aguayoalarcon@itismajo.school>
2025-06-11 09:50:22 +0200

    TrisV.1
    Inserimento file e creazione interfaccia. (Gioco incompleto)

---
+++ game.js
@@ -0,0 +1,9 @@
+// Gestione turni
+let turn = 0; // 0 per player X, 1 per player O
+// Griglia del gioco
+// Inizializza la griglia come una matrice 3x3 vuota
+let grid = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
